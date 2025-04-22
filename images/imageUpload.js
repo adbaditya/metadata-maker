c8ba4c2a51e3a5908c2d7e2b6b5e0e681ade3375
Aditya Bajaj <adb.aditya@gmail.com>
2025-04-22 16:15:05 +0530

    field id fix for notes

--- images/imageUpload.js
+++ images/imageUpload.js
@@ -394,7 +394,7 @@ async function preFetchFromAI() {
                 : (metadata.publisher || '');
 
             document.getElementById('year').value = metadata.publicationDate || '';
-            document.getElementById('synopsis-text').value = metadata.synopsisOfBook || '';
+            document.getElementById('notes').value = metadata.synopsisOfBook || '';
 
             if (metadata.authors?.length > 0) {
                 document.getElementById('family_name').value = metadata.authors[0].familyName || '';
