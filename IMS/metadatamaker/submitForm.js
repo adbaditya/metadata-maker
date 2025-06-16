b412e0efbc803b43bdb6dd099b5a04621559c0a6
Aditya Bajaj <adb.aditya@gmail.com>
2025-06-16 06:22:59 +0530

    FIX for insti details

--- IMS/metadatamaker/submitForm.js
+++ IMS/metadatamaker/submitForm.js
@@ -92,15 +92,15 @@ function getnamesubfields(lcuri){
 function generateInstitutionInfo() {
 	var output = {
 		//040 $a, 040 $c
-		marc: 'UIU',
+		marc: 'VCU',
 		mods: {
-			physicalLocation: 'University of Illinois at Urbana-Champaign, Library',
-			recordContentSource: 'UIU'
+			physicalLocation: 'Art & Design Library',
+			recordContentSource: 'D@A'
 		},
 		//"seller" info
 		html: {
-			url: 'http://id.loc.gov/authorities/names/n79066210',
-			name: 'University of Illinois at Urbana-Champaign'
+			url: 'http://id.loc.gov/authorities/names/qadovcu',
+			name: 'Virginia Commonwealth University, Qatar'
 		}
 	};
 
